92ddd03a475f25966b460607b30dc9c180b60f41
Professor Haris <professorharis804@gmail.com>
2026-01-10 02:44:39 -0800

    Fix: Image path and case sensitivity

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,21 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Isse WASM files ko load karne ki ijazat milti hai
-  webpack: (config) => {
-    config.experiments = { ...config.experiments, asyncWebAssembly: true };
-    return config;
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
-          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
-        ],
-      },
-    ];
+const config = {
+  plugins: {
+    "@tailwindcss/postcss": {},
   },
 };
 
-export default nextConfig;
+export default config;
