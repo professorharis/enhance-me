f81f977e494c5f9e0abe416ceb76998538d3fd3d
Professor Haris <professorharis804@gmail.com>
2026-01-15 02:13:10 -0800

    Fixed next.config.js syntax and allowed external images

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-images: {
-  remotePatterns: [
-    {
-      protocol: 'https',
-      hostname: 'i.ibb.co',
-    },
-  ],
-},
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
